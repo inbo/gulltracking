369cc2c216d8f2940df063142ee7d6d349c2ed72
bartaelterman <bart.aelterman@gmail.com>
2015-11-10 16:30:40 +0100

    add example data

--- R/data.R
+++ R/data.R
@@ -11,3 +11,21 @@
 #' 
 #' @format character vector
 "species_choices"
+
+#' Tracks validated
+#' 
+#' An example data table containing validated bird tracking data (only 100
+#' records). This is what you can expect after successfully loading and
+#' validating tracking data.
+#' 
+#' @format data table
+"tracks_validated"
+
+#' Birds validated
+#' 
+#' An example data table containing validated bird metadata (only 2 records).
+#' This is what you can expect after successfully loading and validating bird
+#' metadata.
+#' 
+#' @format data table
+"birds_validated"
