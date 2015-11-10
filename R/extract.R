cfa4f8122219cbe348aab851ed4fb40d92613bba
bartaelterman <bart.aelterman@gmail.com>
2015-11-10 13:04:01 +0100

    split functions + extract bird metadata

--- R/extract.R
+++ R/extract.R
@@ -24,18 +24,35 @@ check_numeric_values <- function(colname, col) {
 #'
 #' @param filename The file containing bird tracking data in csv
 #' format. (','-delimited, header included)
-#' @return A dataframe containing the tracking data
+#' @return A data table (not a data frame!) containing the tracking data
 #' @examples
 #' \dontrun{
 #' load_tracks_file(inputFile)
 #' }
 #' @import data.table
 load_tracks_file <- function(filename) {
-	issues <- c()
 	data <- fread(filename, dec=".", header=TRUE, sep=",", na.strings=c("\\N"))
-	
+}
+
+
+#' Validate tracking data
+#' @description Validate the data coming either from a csv file or
+#' from the UvA-BiTS virtual lab directly.
+#' 
+#' @param tracks_data The tracking data as a data table
+#' @return validated tracking data as a data table if no errors are found.
+#' @examples
+#' \dontrun{
+#' validate_tracks_data(tracking_data)
+#' }
+validate_tracks_data <- function(tracks_data)	{
+	issues <- c()
 	# set data types for non-character columns
-	data[, date_time:=lubridate::ymd_hms(date_time)]
+	nas_in_date_time <- sum(is.na(tracks_data$date_time))
+	tracks_data[, date_time:=lubridate::ymd_hms(date_time)]
+	if (sum(is.na(tracks_data$date_time)) > nas_in_date_time) {
+		issues <- c(issues, "unparsable values found in column date_time")
+	}
 	
 	# check whether columns can be converted to numeric
 	numeric_cols <- c("latitude", "longitude", "altitude", "pressure", "temperature",
@@ -44,40 +61,136 @@ load_tracks_file <- function(filename) {
 										"speed_3d", "speed_2d", "direction", "altitude_agl")
 	lapply(numeric_cols, function(x) {
 		tryCatch({
-			print(paste(x, "..."))
-			check_numeric_values(x, data[[x]])
+			check_numeric_values(x, tracks_data[[x]])
 		}, error=function(e) {
-					print(paste("non numeric values found in column ", x))
+				issues <<- append(issues, paste("non numeric values found in column ", x))
 		}
 		)
 	})
-	if (length(issues) > 0) {
-		print(issues)
-		stop()
-	}
 	
 	# convert to numeric columns
-	data[, latitude:=as.numeric(latitude)]
-	data[, longitude:=as.numeric(longitude)]
-	data[, altitude:=as.numeric(altitude)]
-	data[, pressure:=as.numeric(pressure)]
-	data[, temperature:=as.numeric(temperature)]
-	data[, satellites_used:=as.numeric(satellites_used)]
-	data[, gps_fixtime:=as.numeric(gps_fixtime)]
-	data[, positiondop:=as.numeric(positiondop)]
-	data[, h_accuracy:=as.numeric(h_accuracy)]
-	data[, v_accuracy:=as.numeric(v_accuracy)]
-	data[, x_speed:=as.numeric(x_speed)]
-	data[, y_speed:=as.numeric(y_speed)]
-	data[, z_speed:=as.numeric(z_speed)]
-	data[, speed_accuracy:=as.numeric(speed_accuracy)]
-	data[, speed_3d:=as.numeric(speed_3d)]
-	data[, speed_2d:=as.numeric(speed_2d)]
-	data[, direction:=as.numeric(direction)]
-	data[, altitude_agl:=as.numeric(altitude_agl)]
+	tracks_data[, latitude:=as.numeric(latitude)]
+	tracks_data[, longitude:=as.numeric(longitude)]
+	tracks_data[, altitude:=as.numeric(altitude)]
+	tracks_data[, pressure:=as.numeric(pressure)]
+	tracks_data[, temperature:=as.numeric(temperature)]
+	tracks_data[, satellites_used:=as.numeric(satellites_used)]
+	tracks_data[, gps_fixtime:=as.numeric(gps_fixtime)]
+	tracks_data[, positiondop:=as.numeric(positiondop)]
+	tracks_data[, h_accuracy:=as.numeric(h_accuracy)]
+	tracks_data[, v_accuracy:=as.numeric(v_accuracy)]
+	tracks_data[, x_speed:=as.numeric(x_speed)]
+	tracks_data[, y_speed:=as.numeric(y_speed)]
+	tracks_data[, z_speed:=as.numeric(z_speed)]
+	tracks_data[, speed_accuracy:=as.numeric(speed_accuracy)]
+	tracks_data[, speed_3d:=as.numeric(speed_3d)]
+	tracks_data[, speed_2d:=as.numeric(speed_2d)]
+	tracks_data[, direction:=as.numeric(direction)]
+	tracks_data[, altitude_agl:=as.numeric(altitude_agl)]
 	
 	# drop unused columns
 	# ... location: this is a binary blob from a geometric data type. Cannot be used.
-	data[, location:=NULL]
-	return(data)
+	tracks_data[, location:=NULL]
+	
+	if (length(issues) > 0) {
+		print(paste(issues, sep="\n"))
+		stop("Validation failed")
+	}
+	return(tracks_data)
+}
+
+#' Load bird metadata file
+#' @description Load a file containing bird metadata. This file is managed at the
+#' INBO using CartoDB Create a csv export of that file, and make sure it is
+#' "," delimited.
+#' 
+#' @param filename The name of the file containing bird metadata
+#' @return A data table (not a data frame!) containing the bird metadata
+#' @examples 
+#' \dontrun{
+#' load_bird_file(inputFile)
+#' }
+#' @import data.table
+load_bird_file <- function(filename) {
+	data <- fread(filename, dec=".", header=TRUE, sep=",")
+}
+
+
+#' Validate bird data
+#' @description Validate the bird metadata
+#' 
+#' @param bird_data The bird metadata as a data table
+#' @return validated bird metadata as a data table if no errors are found.
+#' @export
+validate_bird_data <- function(bird_data) {
+	issues <- c()
+	# set date time data types
+	nas_in_tr_start_time <- sum(is.na(bird_data$tracking_start_date_time))
+	bird_data[, tracking_start_date_time:=lubridate::ymd_hms(tracking_start_date_time)]
+	if (sum(is.na(bird_data$tracking_start_date_time)) > nas_in_tr_start_time) {
+		issues <- c(issues, "unparsable values found in column tracking_start_date_time")
+	}
+	nas_in_tr_end_time <- sum(is.na(bird_data$tracking_end_date_time))
+	bird_data[, tracking_end_date_time:=lubridate::ymd_hms(tracking_end_date_time)]
+	if (sum(is.na(bird_data$tracking_end_date_time)) > nas_in_tr_end_time) {
+		issues <- c(issues, "unparsable values found in column tracking_end_date_time")
+	}
+	nas_in_created_at <- sum(is.na(bird_data$created_at))
+	bird_data[, created_at:=lubridate::ymd_hms(created_at)]
+	if (sum(is.na(bird_data$created_at)) > nas_in_created_at) {
+		issues <- c(issues, "unparsable values found in column created_at")
+	}
+	nas_in_updated_at <- sum(is.na(bird_data$updated_at))
+	bird_data[, updated_at:=lubridate::ymd_hms(updated_at)]
+	if (sum(is.na(bird_data$updated_at)) > nas_in_updated_at) {
+		issues <- c(issues, "unparsable values found in column created_at")
+	}
+	
+	# convert enumeration columns to factors
+	sex_choices <- c("male", "female")
+	bird_data[, sex:=as.factor(sex)]
+	if (sum(sort(levels(bird_data$sex)) != sort(sex_choices)) > 0) {
+			issues <- append(issues, paste("value found in column sex that does not match one of: ",
+									paste(sex_choices, collapse=", ")
+									)
+						)
+	}
+	species_choices <- c("Larus argentatus", "Larus fuscus")
+	bird_data[, species:=as.factor(species)]
+	if (sum(sort(levels(bird_data$species)) != sort(species_choices)) > 0) {
+			issues <- append(issues, paste("value found in column species that does not match one of: ",
+									paste(species_choices, collapse=", ")
+						  	)
+			)
+	}
+	
+	# check whether columns can be converted to numeric
+	numeric_cols <- c("device_info_serial", "weight_in_g", "colony_latitude",
+										"colony_longitude")
+	lapply(numeric_cols, function(x) {
+		tryCatch({
+			check_numeric_values(x, bird_data[[x]])
+		}, error=function(e) {
+			issues <<- append(issues, paste("non numeric values found in column ", x))
+		}
+		)
+	})
+	
+	# convert to numeric columns
+	bird_data[, device_info_serial:=as.numeric(device_info_serial)]
+	bird_data[, weight_in_g:=as.numeric(weight_in_g)]
+	bird_data[, colony_latitude:=as.numeric(colony_latitude)]
+	bird_data[, colony_longitude:=as.numeric(colony_longitude)]
+	
+	# drop unused columns
+	# ... cartodb_id: this id has no use outside of CartoDB.
+	# ... the_geom: this is a binary blob from a geometric data type. Cannot be used.
+	bird_data[, cartodb_id:=NULL]
+	bird_data[, the_geom:=NULL]
+	
+	if (length(issues) > 0) {
+		print(paste(issues, sep="\n"))
+		stop("Validation failed")
+	}
+	return(bird_data)
 }
--- tests/testthat/test_extract.R
+++ tests/testthat/test_extract.R
@@ -1,6 +1,103 @@
+# -------------------------------------------- # 
+#      Fixture data
+# -------------------------------------------- # 
+fixture_tracking_data <- data.table(
+	device_info_serial=c("847", "322", "12"),
+	date_time=c("2013-05-31 16:31:31", "2013-05-31 16:32:34", "2013-05-31 16:33:37"),
+	latitude=c("51.3416", "51.3413", "51.3244"),
+	longitude=c("3.1738", "3.1734", "3.17487"),
+	altitude=c("10", "11", "2"),
+	pressure=c("1930", "3839", "222"),
+	temperature=c("12", "10.4", "23"),
+	satellites_used=c("3", "4", "4"),
+	gps_fixtime=c("31.323", "232.424", "32.3239"),
+	positiondop=c("4.424", "5.324", "8.32183"),
+	h_accuracy=c("8.42", "0.3723", "32.33"),
+	v_accuracy=c("8.42", "0.3723", "32.33"),
+	x_speed=c("8.42", "0.3723", "32.33"),
+	y_speed=c("8.42", "0.3723", "32.33"),
+	z_speed=c("8.42", "0.3723", "32.33"),
+	speed_accuracy=c("8.42", "0.3723", "32.33"),
+	location=c("84KSE89SNF84HQ", "84KSE89SNF84HQ", "84KSE89SNF84HQ"),
+	userflag=c("0", "1", "1"),
+	speed_3d=c("8.42", "0.3723", "32.33"),
+	speed_2d=c("8.42", "0.3723", "32.33"),
+	direction=c("8.42", "0.3723", "32.33"),
+	altitude_agl=c("8.42", "0.3723", "32.33")
+)
+
+fixture_bird_data <- data.table(
+	device_info_serial=c("783", "328", "2"),
+	ring_code=c("32J", "2DZ", "238H"),
+	color_ring_code=c("HDUE", "JDUS", "DUS"),
+	species=c("Larus fuscus", "Larus fuscus", "Larus argentatus"),
+	sex=c("male", "female", "female"),
+	weight_in_g=c("738", "481", "1953"),
+	tracking_start_date_time=c("2013-05-27 18:00:00+00",
+														 "2013/05/27 18:00:00",
+														 "2013-05-27 20:00:00+02"),
+	tracking_end_date_time=c("2013-05-27 18:00:00+00",
+													 "2013/05/27 18:00:00",
+													 "2013-05-27 20:00:00+02"),
+	colony_latitude=c("51.3493", "51.334", "51.356"),
+	colony_longitude=c("2.593", "3.2113", "2.904"),
+	remarks=c("", "", "nothing to say"),
+	cartodb_id=c("doesn't matter", "", ""),
+	created_at=c("2013-05-27 18:00:00+00",
+							 "2013/05/27 18:00:00",
+							 "2013-05-27 20:00:00+02"),
+	updated_at=c("2013-05-27 18:00:00+00",
+							 "2013/05/27 18:00:00",
+							 "2013-05-27 20:00:00+02"),
+	the_geom=c("IQE9JF93Q3JFIQ", "3IQH83F3NQI", "EIHQ83NQICE"),
+	bird_name=c("Wilma", "Hilbran", "Joke"),
+	colony_name=c("Vismijn", "APM", "APM"),
+	colony_location=c("Oostende", "Zeebrugge", "Zeebrugge")
+)
+
+# -------------------------------------------- # 
+#      Tests
+# -------------------------------------------- # 
 test_that("non-numeric values throw an error, but NA's are ignored", {
 	expect_equal(check_numeric_values("testcol", c(1, 2, 3)), c(1, 2, 3))
 	expect_equal(check_numeric_values("testcol", c(1, 2, NA)), c(1, 2, NA))
 	expect_equal(check_numeric_values("testcol", c(NA, NA, NA)), c(NA, NA, NA))
 	expect_error(check_numeric_values("testcol", c(1, 2, "X")))
+})
+
+test_that("bird tracking validation returns data if no errors are found", {
+  expect_is(validate_tracks_data(fixture_tracking_data), "data.table")
+})
+
+test_that("bird tracking validation stops if an error is found", {
+	# skip("too much output") # DELETE LATER
+	error_data <- copy(fixture_tracking_data)
+	error_data$date_time <- c("2013-05-31 16:31:31",
+														"2013-05-31 16:32:34",
+														"31/05/2013 16:33:37") # uh oh!
+	expect_error(validate_tracks_data(error_data))
+	error_data <- copy(fixture_tracking_data)
+	error_data$altitude <- c("4", "5", "a")
+	expect_error(validate_tracks_data(error_data))
+})
+
+test_that("bird metadata validation returns data if no errors are found", {
+	expect_is(validate_bird_data(fixture_bird_data), "data.table")
+})
+
+test_that("bird metadata validation stops if an error is found", {
+	error_data <- copy(fixture_bird_data)
+	error_data$device_info_serial <- c("a", "b", "1")
+	expect_error(validate_bird_data(error_data))
+	error_data <- copy(fixture_bird_data)
+	error_data$created_at <- c("2013-05-31 16:31:31",
+														 "2013-05-31 16:32:34",
+														 "31/05/2013 16:33:37")
+	expect_error(validate_bird_data(error_data))
+	error_data <- copy(fixture_bird_data)
+	error_data$sex <- c("male", "female", "unknown")
+	expect_error(validate_bird_data(error_data))
+	error_data <- copy(fixture_bird_data)
+	error_data$species <- c("Larus fuscus", "Larus fuscus", "unknown")
+	expect_error(validate_bird_data(error_data))
 })
