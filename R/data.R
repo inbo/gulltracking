c971d77e427ffc894245f27f9deab5e25bea36a7
damianooldoni <damiano.oldoni@inbo.be>
2020-11-13 09:31:46 +0100

    Add 2 small datasets to package and document them

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Sample of Movebank GPS data of a Lesser black-backed
+#' gulls bird tracking dataset
+#'
+#' A bird tracking dataset containing 100 rows of GPS data from 2013 of Lesser black-backed gulls.
+#'
+#' @format A tibble (data.frame) with 100 rows and 22 variables.
+#' @source To download the entire file (zip format): \url{https://zenodo.org/record/3968687/files/LBBG_ZEEBRUGGE-gps-2013.csv.zip?download=1}. More information on Zenodo: https://zenodo.org/record/3968687.
+"lbbg_gps"
+
+#' Movebank reference data of a Lesser black-backed gulls bird tracking dataset
+#'
+#' A dataset containing reference data (metadata) of a GPS data from 2013 of lesser black-backed gulls.
+#'
+#' @format A tibble (data.frame) with 100 rows and 24 variables.
+#' @source Reference data in csv format: \url{https://zenodo.org/record/3968687/files/LBBG_ZEEBRUGGE-gps-2013.csv.zip?download=1}. More information on Zenodo: https://zenodo.org/record/3968687.
+"lbbg_reference"
